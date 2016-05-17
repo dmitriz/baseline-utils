5c827f75a8f78953df9a52f67b7cd2d578c9625d
dmitriz <dmitri14@gmail.com>
2016-05-17 18:19:49 +0100

    Complete tests for distance from Edge

--- src/box-offset.js
+++ src/box-offset.js
@@ -1,5 +1,14 @@
 // box-offset.js
 
-module.exports = function () {
-	return 0
+var math = require('better-math')
+
+/**
+ * @fromBase {finite} desired distance from base
+ * @offset {finite} distance between base and the box edge
+ * @return {finite} distance from the box egde
+ */
+module.exports = {
+	fromEdge: function (fromBase, offset) {
+		return math.add(fromBase, -offset)
+	}
 }
--- src/box-offset_test.js
+++ src/box-offset_test.js
@@ -1,9 +1,21 @@
 // box-offset_test.js
 
-var fromEdge = require('./box-offset.js')
+var fromEdge = require('./box-offset.js').fromEdge
 
-describe('Offset', function () {
-	it('should compute distance from the box Edge', function () {
+describe('Compute distance from the box Edge', function () {
+	it('should be 0 when desired distance and offset are 0', function () {
 		expect(fromEdge(0, 0)).toBe(0)
+	})
+	it('should be the same as desired distance if offset is 0', function () {
+		expect(fromEdge(1.2, 0)).toBe(1.2)
+		expect(fromEdge(121, 0)).toBe(121)
+	})
+	it('should be the negative offset if desired distance is 0', function () {
+		expect(fromEdge(0, 0.33)).toBe(-0.33)		
+		expect(fromEdge(0, 33)).toBe(-33)		
+	})
+	it('should reduce the desired distance by the offset', function () {
+		expect(fromEdge(1.12, 1.12)).toBe(0)
+		expect(fromEdge(4.12, 1.13)).toBe(2.99)
 	})
 })
