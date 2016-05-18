63168c7a485d736a2671dbdf6dcaaf6e11a80b52
dmitriz <dmitri14@gmail.com>
2016-05-18 06:17:06 +0100

    Standard fix

--- src/box-offset.js
+++ src/box-offset.js
@@ -1,20 +1,22 @@
 // box-offset.js
 
+'use strict'
+
 var math = require('better-math')
 
 module.exports = {
-	/**
-	 * @fromBase {finite} desired distance from base
-	 * @offset {finite} distance between base and the box edge
-	 *
-	 * @return {finite} distance from the box egde
-	 */
-	fromEdge: function (fromBase, offset) {
-		return math.add(fromBase, -offset)
-	},
-	offset: function (lineHeight, baseOffset) {
-		var leading = math.add(lineHeight, -1)
-		var halfLeading = leading / 2
-		return math.add(halfLeading, baseOffset)
-	}
+  /**
+   * @fromBase {finite} desired distance from base
+   * @offset {finite} distance between base and the box edge
+   *
+   * @return {finite} distance from the box egde
+   */
+  fromEdge: function (fromBase, offset) {
+    return math.add(fromBase, -offset)
+  },
+  offset: function (lineHeight, baseOffset) {
+    var leading = math.add(lineHeight, -1)
+    var halfLeading = math.div(leading, 2)
+    return math.add(halfLeading, baseOffset)
+  }
 }
--- src/box-offset_test.js
+++ src/box-offset_test.js
@@ -6,28 +6,28 @@ var fromEdge = box.fromEdge
 var offset = box.offset
 
 describe('Distance from the box Edge', function () {
-	it('should be 0 when desired distance and offset are 0', function () {
-		expect(fromEdge(0, 0)).toBe(0)
-	})
-	it('should be the same as desired distance if offset is 0', function () {
-		expect(fromEdge(1.2, 0)).toBe(1.2)
-		expect(fromEdge(121, 0)).toBe(121)
-	})
-	it('should be the negative offset if desired distance is 0', function () {
-		expect(fromEdge(0, 0.33)).toBe(-0.33)		
-		expect(fromEdge(0, 33)).toBe(-33)		
-	})
-	it('should reduce the desired distance by the offset', function () {
-		expect(fromEdge(1.12, 1.12)).toBe(0)
-		expect(fromEdge(4.12, 1.13)).toBe(2.99)
-	})
+  it('should be 0 when desired distance and offset are 0', function () {
+    expect(fromEdge(0, 0)).toBe(0)
+  })
+  it('should be the same as desired distance if offset is 0', function () {
+    expect(fromEdge(1.2, 0)).toBe(1.2)
+    expect(fromEdge(121, 0)).toBe(121)
+  })
+  it('should be the negative offset if desired distance is 0', function () {
+    expect(fromEdge(0, 0.33)).toBe(-0.33)
+    expect(fromEdge(0, 33)).toBe(-33)
+  })
+  it('should reduce the desired distance by the offset', function () {
+    expect(fromEdge(1.12, 1.12)).toBe(0)
+    expect(fromEdge(4.12, 1.13)).toBe(2.99)
+  })
 })
 
 describe('Total bottom offset', function () {
-	it('should be 0 when line-height is 1 and base offset is 0', function () {
-		expect(offset(1, 0)).toBe(0)
-	})
-	it('should equal half-leading when base offset is 0', function () {
-		expect(offset(2, 0)).toBe(0.5)
-	})
+  it('should be 0 when line-height is 1 and base offset is 0', function () {
+    expect(offset(1, 0)).toBe(0)
+  })
+  it('should equal half-leading when base offset is 0', function () {
+    expect(offset(2, 0)).toBe(0.5)
+  })
 })
